a5e182560c0fcf0e1ea03d8d273a304a96508547
Money00111 <niyigenaepizo9@gmail.com>
2026-07-23 18:35:31 +0200

    Add Firebase configuration and initialization

---
+++ services/firebase.js
@@ -0,0 +1,54 @@
+// ======================================
+// RWANDA Me&You
+// Firebase Configuration
+// ======================================
+
+
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+
+
+import { getAuth } 
+from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+
+import { getFirestore } 
+from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+
+
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyA9k_ADHHKlF6kJ21_VL91LEVxzXyrhJUs",
+
+authDomain:
+"rwanda-me-you.firebaseapp.com",
+
+projectId:
+"rwanda-me-you",
+
+storageBucket:
+"rwanda-me-you.appspot.com",
+
+messagingSenderId:
+"1045065051966",
+
+appId:
+"1:1045065051966:web:343afa4e425bda7efac66f"
+
+};
+
+
+
+const app = initializeApp(firebaseConfig);
+
+
+
+export const auth = getAuth(app);
+
+
+export const db = getFirestore(app);
+
+
+
+export default app;
