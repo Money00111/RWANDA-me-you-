e84f606ff25954238eb75c4d560b8aa41f87f98a
Money00111 <niyigenaepizo9@gmail.com>
2026-07-23 18:36:30 +0200

    Add authentication service for user management
    Implement authentication service with user registration, login, logout, and password reset functionalities.

---
+++ services/authService.js
@@ -0,0 +1,178 @@
+// ======================================
+// RWANDA Me&You
+// Authentication Service
+// ======================================
+
+
+import {auth, db}
+from "./firebase.js";
+
+
+
+import {
+
+createUserWithEmailAndPassword,
+
+signInWithEmailAndPassword,
+
+signOut,
+
+sendPasswordResetEmail
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+
+
+import {
+
+doc,
+
+setDoc
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+
+
+
+
+// ===============================
+// REGISTER USER
+// ===============================
+
+export async function registerUser(
+name,
+email,
+password
+){
+
+
+const result =
+
+await createUserWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
+
+
+
+const user = result.user;
+
+
+
+await setDoc(
+
+doc(db,"users",user.uid),
+
+{
+
+
+uid:user.uid,
+
+name:name,
+
+email:email,
+
+photo:"",
+
+bio:"",
+
+createdAt:new Date()
+
+
+}
+
+);
+
+
+
+return user;
+
+
+}
+
+
+
+
+
+
+// ===============================
+// LOGIN USER
+// ===============================
+
+
+export async function loginUser(
+email,
+password
+){
+
+
+const result =
+
+await signInWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
+
+
+
+return result.user;
+
+
+}
+
+
+
+
+
+// ===============================
+// LOGOUT
+// ===============================
+
+
+export async function logoutUser(){
+
+
+await signOut(auth);
+
+
+}
+
+
+
+
+
+// ===============================
+// RESET PASSWORD
+// ===============================
+
+
+export async function resetUserPassword(email){
+
+
+await sendPasswordResetEmail(
+
+auth,
+
+email
+
+);
+
+
+}
