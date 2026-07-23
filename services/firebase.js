f101f59f17948af445cc07d684cecaebe969fe91
Money00111 <niyigenaepizo9@gmail.com>
2026-07-23 20:25:25 +0200

    Add Firebase Storage integration and update config

--- services/firebase.js
+++ services/firebase.js
@@ -16,38 +16,65 @@ import { getFirestore }
 from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
 
+import { getStorage } 
+from "https://www.gstatic.com/firebasejs/11.9.1/firebase-storage.js";
+
+
+
+
+// Firebase Config
 
 const firebaseConfig = {
 
-apiKey: "AIzaSyA9k_ADHHKlF6kJ21_VL91LEVxzXyrhJUs",
+apiKey:
+"AIzaSyC2Ov1i3RKve-RxJ5vXN8KssPqau7PX3BA",
+
 
 authDomain:
-"rwanda-me-you.firebaseapp.com",
+"rwanda-me-you-55cdf.firebaseapp.com",
+
 
 projectId:
-"rwanda-me-you",
+"rwanda-me-you-55cdf",
+
 
 storageBucket:
-"rwanda-me-you.appspot.com",
+"rwanda-me-you-55cdf.firebasestorage.app",
+
 
 messagingSenderId:
-"1045065051966",
+"859478943706",
+
 
 appId:
-"1:1045065051966:web:343afa4e425bda7efac66f"
+"1:859478943706:web:0dedafe6c7bd142a0802e3",
+
+
+measurementId:
+"G-8J4XZ9HZW6"
 
 };
 
 
 
+
+
+// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 
+
+
+// Services
 
 export const auth = getAuth(app);
 
 
 export const db = getFirestore(app);
+
+
+export const storage = getStorage(app);
 
 
 
